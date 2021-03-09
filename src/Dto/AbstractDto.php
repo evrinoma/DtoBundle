59d63128ed91d6c95f4a2eef847afe0b418da32b
evrinoma <evrinoma@gmail.com>
2021-03-09 19:58:46 +0300

    Dto Request prototype logic

--- src/Dto/AbstractDto.php
+++ src/Dto/AbstractDto.php
@@ -2,8 +2,26 @@
 
 namespace Evrinoma\DtoBundle\Dto;
 
+use Symfony\Component\HttpFoundation\Request;
+
 abstract class AbstractDto implements DtoInterface
 {
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @return Request
+     */
+    public function getCloneRequest():Request
+    {
+        if (!$this->request) {
+            $this->request = new Request();
+        }
+
+        return clone $this->request;
+    }
 //region SECTION: Dto
     /**
      * @return DtoInterface
